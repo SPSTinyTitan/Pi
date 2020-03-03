6d173c5dd0562a8adb47f0c4fdf86244902fae05
kokowang <kokowang5699@gmail.com>
2020-03-02 23:06:37 -0500

    Add comparisons

--- yuke/edge_detect.py
+++ yuke/edge_detect.py
@@ -17,6 +17,12 @@ class edge_detect:
         self.coarse_size = (int)(size/100)
         self.grid = (torch.zeros((1, 1, size, size))).float()
         self.coarse = (torch.zeros((1, 1, self.coarse_size, self.coarse_size))).float()
+        # Some error is contributed from edge effects, padding can fix this
+        # However, this error is 2-3 orders of magnitude smaller than any given precision
+        # of this algorithm. The correction is slow and unnessacary.
+
+        # self.max = (1. + 1./self.size)
+        self.max = 1
 
     def circle(self, size, topleft, bottomright, padding=1):
         #Create grid padded so convolution returns correct dimensions
@@ -32,8 +38,10 @@ class edge_detect:
         total = 0
         self.grid = torch.abs(self.calc_fine())
         self.grid = self.grid / self.grid.sum(3, keepdim=True)
+        # removing nan
+        # self.grid[self.grid != self.grid] = 0
         ind = torch.nonzero(self.grid)
-        factor = 1./(self.size**2)
+        factor = (self.max**2)/(self.size**2)
         print(ind.size(0))
         for i in range(ind.size(0)):
             x = ind[i,2]
@@ -41,27 +49,31 @@ class edge_detect:
             total += self.grid[0,0,x,y] * y
         return total * factor * 4
 
-
-    #Clean this up
+    # Clean this up
     def calc_fine(self):
         self.calc_coarse()
         ind = torch.nonzero(self.coarse)
         kernel = torch.tensor([[-1, -1, -1], [-1, 8., -1], [-1, -1, -1]]).view((1, 1, 3, 3))
+        factor = self.max/self.size
         for i in range(ind.size(0)):
             topleft = [100 * ind[i, 2], 100 * ind[i, 3]]
             bottomright = [topleft[0] + 100, topleft[1] + 100]
-            factor = 1./self.size
             sample = self.circle(100, (factor*topleft[0], factor*topleft[1]), (factor*bottomright[0], factor*bottomright[1]), 1)
             sample = torch.conv2d(sample, kernel, stride=1)
             self.grid[0, 0, topleft[0]:bottomright[0], topleft[1]:bottomright[1]] = sample
         return self.grid
 
     def calc_coarse(self):
-        sample = self.circle(self.coarse_size, (0,0), (1, 1), 1)
+        sample = self.circle(self.coarse_size, (0,0), (self.max, self.max), 1)
         kernel = torch.tensor([[-1, -1, -1], [-1, 8., -1], [-1, -1, -1]]).view((1, 1, 3, 3))
         sample = torch.conv2d(sample, kernel, stride=1)
         self.coarse = torch.abs(sample)
         return self.coarse
+
+    def calc_raw(self):
+        sample = self.circle(self.size, (0,0), (self.max, self.max), 0)
+        total = torch.sum(sample)
+        return 4 * total / (self.size ** 2) 
 
     def update(self):
         self.grid = self.grid + self.calc(0.1)
--- yuke/main.py
+++ yuke/main.py
@@ -2,16 +2,30 @@ from time import perf_counter
 import edge_detect
 import torch
 import cv2
+import math
 
 
-
-size = 20000
-
+#Create 10000x10000 grid (100m samples)
+size = 10000
 circle = edge_detect.edge_detect(size)
-
+torch.set_printoptions(precision=10)
+#Edge detection
 t1 = perf_counter()
 result = circle.calc_fine()
-t2 = perf_counter()
-print("Elapsed time:", t2 - t1, "s")
-print(circle.calc_pi())
+pi = circle.calc_pi()
+t4 = perf_counter()
+print("Pi =", pi)
+print("Error:", math.pi - pi)
+print("Total time for pi using edge detection:", t4 - t1)
+
+#Calculating pi without edge detection
+t5 = perf_counter()
+pi = circle.calc_raw()
+t6 = perf_counter()
+print("Pi =", pi)
+print("Error:", math.pi - pi)
+print("Total time for pi without edge detection:", t6 - t5)
+
+
+#Draw the detected edge
 #edge_detect.draw(result)
